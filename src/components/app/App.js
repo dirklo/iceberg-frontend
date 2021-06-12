d67d3b7be37539c3057f3203bc3d426552e49101
rohanphillips <rohan@solutions-insurance.com>
2021-06-11 22:15:35 -0500

    set up customPath

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,15 +8,30 @@ import './App.css';
 import UsersContainer from '../users/UsersContainer'
 import React from 'react';
 
-function App() {
+class DebugRouter extends Router {
+  constructor(props){
+    super(props);
+    console.log('initial history is: ', JSON.stringify(this.history, null,2))
+    this.history.listen((location, action)=>{
+      console.log(
+        `The current URL is ${location.pathname}${location.search}${location.hash}`
+      )
+      console.log(`The last navigation action was ${action}`, JSON.stringify(this.history, null,2));
+    });
+  }
+}
+
+function App() {  
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-        </Route>
-        <Route path="/users" render={routerProps => <UsersContainer {...routerProps}/>}/>
-      </Switch>
-    </Router>
+    <DebugRouter>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+          </Route>
+          <Route path="/:customPath" render={routerProps => <UsersContainer {...routerProps}/>}/>
+        </Switch>
+      </Router>
+    </DebugRouter>
   );
 }
 
