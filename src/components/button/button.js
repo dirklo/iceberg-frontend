2c4b5035e3d0ab550703e20bc37746991d238b56
Stacey McKnight <mcknight.stm@gmail.com>
2021-06-12 17:01:31 -0500

    add button component

---
+++ src/components/button/button.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './button.module.css';
+
+function Button({onClick, style, text}) {
+
+  const btnStyle = styles[`${style}`]
+
+  return (
+    <button 
+      className={`${styles.Btn} ${btnStyle}`}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  )
+}
