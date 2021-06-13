3d4fbe9a6be74a0a172e475d86cdfbc2498df79a
Stacey McKnight <mcknight.stm@gmail.com>
2021-06-13 14:08:14 -0500

    comment out hobbies and foods components

--- src/components/hobbies/AddHobby.js
+++ src/components/hobbies/AddHobby.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import CreatableSelect from 'react-select/creatable';
+// import React from 'react'
+// import CreatableSelect from 'react-select/creatable';
 
-export default function AddHobby({ hobbiesList, addHobby }) {
-  const searchList = hobbiesList.map(
-    (hobby) => {
-      return {
-        value: hobby.id,
-        label: hobby.name
-      }
-    }
-  )
+// export default function AddHobby({ hobbiesList, addHobby }) {
+//   const searchList = hobbiesList.map(
+//     (hobby) => {
+//       return {
+//         value: hobby.id,
+//         label: hobby.name
+//       }
+//     }
+//   )
   
-  const test = (e) => {
-    addHobby(e.label);
-  }
-  return (
-    <div>
-      <h4>AddHobby</h4>
-      <CreatableSelect 
-        options={searchList}
-        onChange={(e) => test(e)}
-      />
-    </div>
-  )
-}
+//   const test = (e) => {
+//     addHobby(e.label);
+//   }
+//   return (
+//     <div>
+//       <h4>AddHobby</h4>
+//       <CreatableSelect 
+//         options={searchList}
+//         onChange={(e) => test(e)}
+//       />
+//     </div>
+//   )
+// }
