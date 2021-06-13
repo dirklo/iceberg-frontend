d37cad765eb209dbe35216b6a1f4c0c569cbb4df
Stacey McKnight <mcknight.stm@gmail.com>
2021-06-12 21:52:28 -0500

    add user profile image component

---
+++ src/components/users/ProfileImage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Sample from '../../assets/sample-profile.png'; 
+import styles from './ProfileImage.module.css';
+
+function ProfileImage({image, name}) {
+  return (
+    <div className={styles.ProfileImage}>
+      <img src={image} alt={`Photo of ${name}`}></img>
+    </div>
+  )
+}
+
+ProfileImage.defaultProps = {
+  image: Sample,
+  name: "Lorainne Lakiense"
+}
+
+export default ProfileImage;
