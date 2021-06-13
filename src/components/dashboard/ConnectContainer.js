01dc6abd383750fdcea6b3488f04294114b30dc8
Stacey McKnight <mcknight.stm@gmail.com>
2021-06-13 14:03:36 -0500

    add styles for connect page

--- src/components/dashboard/ConnectContainer.js
+++ src/components/dashboard/ConnectContainer.js
@@ -2,6 +2,10 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import ConnectForm from './ConnectForm'
 import ConnectUsers from './ConnectUsers'
+import styles from './ConnectContainer.module.css';
+import Recommended from '../../assets/recommended.png';
+import spStyles from '../splitpane/SplitPane.module.css';
+
 const ConnectContainer = () => {
 
   const dispatch = useDispatch()
@@ -10,10 +14,16 @@ const ConnectContainer = () => {
     fetch("http://localhost:3001/users").then(resp => resp.json()).then(users => dispatch({type: "FETCH_USERS", payload: users}))
   )  
   return (
-      <div id="connect-container">
-        <h1>Connect</h1>
-        <ConnectForm />
-        <ConnectUsers />
+      <div className={styles.ConnectContainer}>
+        <div className={styles.Recommended}>
+          <h1>Recommended Connections</h1>
+          <img className={styles.SampleList} src={Recommended} />
+        </div>
+        <div className={styles.ConnectSearch}>
+          <h1>Connect</h1>
+          <ConnectForm />
+          <ConnectUsers />
+        </div>
       </div>
   )
 }
