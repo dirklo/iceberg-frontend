8c140f618d85914cc123accadc24d77bbedd4308
Stacey McKnight <mcknight.stm@gmail.com>
2021-06-12 21:55:10 -0500

    hide Users Data h1

--- src/components/users/UsersContainer.js
+++ src/components/users/UsersContainer.js
@@ -5,7 +5,7 @@ import UserProfile from './UserProfile'
 function UsersContainer() {  
     return (
         <div className="container border border-amber-200 w-full">          
-          {<h1>Users Data</h1>}
+          {/* {<h1>Users Data</h1>} */}
           <Route path={`/:customPath/`} render={routerProps => <UserProfile {...routerProps}/>} />
         </div>
     );
