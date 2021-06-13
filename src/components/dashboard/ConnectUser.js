ff75a9aa4a1e38c149234239558385ce8343e892
Stacey McKnight <mcknight.stm@gmail.com>
2021-06-13 14:03:58 -0500

    add styles for search result cards on connect page

--- src/components/dashboard/ConnectUser.js
+++ src/components/dashboard/ConnectUser.js
@@ -1,7 +1,46 @@
 import React from 'react'
+import styles from './ConnectUser.module.css';
 
-export default function ConnectUser(props) {  // Stacey will handle styling
+export default function ConnectUser({user}) {  // Stacey will handle styling
+
+    function setImgNum(min, max) {
+      let x = Math.random() * (max-min + 1);
+      return Math.floor(x) + min;
+    }
+    
+    function setRandomPic() {
+      return (
+        require(`../../assets/profile-images/profile-${setImgNum(1, 10)}.png`)
+      )
+    }
+    
+    function fullName() {
+      return `${user.first_name} ${user.last_name}`
+    }
+
     return (
-        <label>{props.user.first_name}</label> 
+        <div className={styles.SearchResultCard}>
+          <div className={styles.Image}>
+            <img src={setRandomPic()} alt={fullName()}/>
+          </div>
+          <div className={styles.UserDetails}>
+            <h3>{fullName()}</h3>
+            <p>
+              <span className="emphasis">Role: </span>
+              {user.job_title}
+              <span className={styles.Divider}>|</span>
+              <span className="emphasis">Team: </span>
+              {user.team.name} 
+            </p>
+            {/* <p>
+              <span className="emphasis">Role: </span>
+              {user.job_title}
+            </p>
+            <p>
+              <span className="emphasis">Team: </span>
+              {user.team.name}
+            </p> */}
+          </div>
+        </div>
     )
 }
