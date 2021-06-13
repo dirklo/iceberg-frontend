1c879bb17679db4599e68efd25327b7d775ff650
Stacey McKnight <mcknight.stm@gmail.com>
2021-06-13 14:01:58 -0500

    add styles for search list on connect page

--- src/components/dashboard/ConnectUsers.js
+++ src/components/dashboard/ConnectUsers.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import ConnectUser from './ConnectUser'
+import styles from './ConnectUsers.module.css'
+
+
 export default function ConnectUsers() {
 
     const connnectUsers = useSelector(state => state.appReducer.connectUsers)
 
     return (
-        <ul>
+        <ul className={styles.ConnectUserList}>
             {connnectUsers.map(u => <li key={u.id} > <ConnectUser user={u}/> </li>)}
         </ul>
     )
