4a0ccae59cb871f68de72cb4a6d14c7d37180b17
rohanphillips <rohan@solutions-insurance.com>
2021-06-13 10:29:57 -0500

    Ready for merge to main

--- src/components/users/UserProfile.js
+++ src/components/users/UserProfile.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import Foods from '../foods/Foods'
 
 function UserProfile(){
   return (
     <div>
       <h1>UserProfile</h1>
-      {/* added in search-add-component branch */}
-      <Foods/>
     </div>
   )
 }
