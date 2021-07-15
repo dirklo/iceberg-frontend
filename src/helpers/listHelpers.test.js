06c51ac221ad2ccba0cd56a5afe7aec62aeb14b2
rohanphillips <rohan@solutions-insurance.com>
2021-07-15 13:36:53 -0500

    add listHelper testing correct dispatch packets are verified

--- src/helpers/listHelpers.test.js
+++ src/helpers/listHelpers.test.js
@@ -1,10 +1,15 @@
-import { makeAvailableList } from './listHelpers'
+
+import { makeAvailableList, changePacket } from './listHelpers'
 
 const item1 = {id: 1, name: "test"}
 const item2 = {id: 2, name: "test2"}
+const label1 = {id: 1, label: "test"}
 
 const itemList = [item1, item2]
-const userList = []
+let userList = []
+let currentList = []
+let newList = []
+let packet
 
 describe('makeAvailableList', () => {
   it('should return all when userList is empty', () => {
@@ -16,5 +21,29 @@ describe('makeAvailableList', () => {
     expect(list).toHaveLength(1)
     expect(list[0].label).toEqual('test2')
     expect(list[0].value).toEqual(2)
+  })
+})
+
+describe('changePacket', () => {
+  beforeEach(() => {
+    newList = []
+    currentList = []    
+  })
+  it('should create information packet', () => {
+    packet = changePacket(1, newList, currentList)
+    expect(packet.willCreate).toBe(false)
+    expect(packet.willDelete).toBe(false)
+  })
+  it('should initiate willCreate', () => {
+    newList.push(label1)
+    packet = changePacket(1, newList, currentList)
+    expect(packet.willCreate).toBe(true)
+    expect(packet.createPacket.items[0].name).toEqual(label1.label)
+  })
+  it('should initiate willDelete', () => {
+    currentList.push(item1)
+    packet = changePacket(1, newList, currentList)
+    expect(packet.willDelete).toBe(true)
+    expect(packet.deletePacket.items[0]).toEqual(item1.id)
   })
 })
