82bec46ddfcb43e92f07dce3fc63b1034e527967
rohanphillips <rohan@solutions-insurance.com>
2021-07-15 11:05:59 -0500

    reducer cleanup after createSlice refactor

--- src/reducers/food.js
+++ src/reducers/food.js
@@ -20,14 +20,3 @@ const foodsSlice = createSlice({
 
 export const { fetchFoods, addFoods } = foodsSlice.actions
 export default foodsSlice.reducer
-
-// export default function foodsState(state= initialState, action){
-//   switch(action.type) {
-//     case "FETCH_FOODS":
-//       return {...state, foodsLoaded: true, foods: [...action.payload]}
-//     case "ADD_FOODS":
-//      return {...state, foods: [...state.foods, ...action.payload]}
-//     default:
-//       return state;
-//   }
-// }
--- src/reducers/hobby.js
+++ src/reducers/hobby.js
@@ -21,18 +21,3 @@ const hobbiesSlice = createSlice({
 
 export const { fetchHobbies, addHobbies } = hobbiesSlice.actions
 export default hobbiesSlice.reducer
-
-// export default function hobbiesState(state= initialState, action){
-//   switch(action.type) {
-//     case "FETCH_HOBBIES":
-//       return {...state, hobbiesLoaded: true, hobbies: [...action.payload]}
-//     case "ADD_HOBBIES":
-//       let newHobbies = state.hobbies
-//       for(let i = 0; i < action.payload.length; i++)  {
-//         newHobbies.push(action.payload[i])
-//       }
-//       return {...state, hobbies: [...newHobbies]}
-//     default:
-//       return state;
-//   }
-// }
